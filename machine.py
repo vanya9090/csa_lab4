3b6c13f001b52257647604cdcd22ac0d92336444
vanya9090 <ivanmironov27112005@gmail.com>
2025-04-25 12:02:54 +0300

    add ALU

--- machine.py
+++ machine.py
@@ -1,5 +1,6 @@
 from enum import Enum 
 from dataclasses import dataclass
+from typing import Callable, Dict, Any
 
 
 class Signal(Enum):
@@ -45,3 +46,75 @@ class Sel:
         ALU : int = 2
         DATA_REGISTER : int = 3
 
+
+class ALU:
+    class Flags(Enum):
+        ZERO : int = 0
+        CARRY : int = 1
+        OVERFLOW : int = 2
+        NEGATIVE : int = 3
+    
+    class Operations(Enum):
+        ADD : int = 0
+        SUB : int = 1
+        AND : int = 2
+        OR : int = 3
+        MUL : int = 4
+        DIV : int = 5
+        RMD : int = 6
+
+
+    def __init__(self):
+        self.__left_term : int = None
+        self.__right_term : int = None
+        self.result : int = None
+        self.flags : dict[ALU.Flags, bool] = {
+            self.Flags.ZERO : False,
+            self.Flags.CARRY: False,
+            self.Flags.OVERFLOW: False,
+            self.Flags.NEGATIVE: False
+        }
+        self.__operations : dict[ALU.Operations, Callable[[int, int], int]] = {
+            self.Operations.ADD : lambda x, y : x + y,
+            self.Operations.SUB : lambda x, y : x - y,
+            self.Operations.AND : lambda x, y : x and y,
+            self.Operations.OR : lambda x, y : x or y,
+            self.Operations.MUL : lambda x, y : x * y,
+            self.Operations.DIV : lambda x, y : x / y,
+            self.Operations.RMD : lambda x, y : x % y
+        }
+
+    def __set_flags(self) -> None:
+        self.flags[self.Flags.ZERO] = self.result == 0
+        self.flags[self.Flags.NEGATIVE] = self.result < 0
+
+    def latch_left_alu(self, sel : Sel.LeftALU) -> None:
+        assert isinstance(sel, Sel.LeftALU), "selector must be LeftALU selector"
+
+        if sel == Sel.LeftALU.REGISTER:
+            pass
+        if sel == Sel.LeftALU.IMMEDIATE:
+            pass
+    
+    def latch_right_alu(self, sel : Sel.RightALU) -> None:
+        assert isinstance(sel, Sel.RightALU), "selector must be RightALU selector"
+
+        if sel == Sel.RightALU.REGISTER:
+            pass
+        if sel == Sel.RightALU.IMMEDIATE:
+            pass
+        if sel == Sel.RightALU.DATA_REGISTER:
+            pass
+        if sel == Sel.RightALU.PLUS_1:
+            self.__right_term = 1
+        if sel == Sel.RightALU.MINUS_1:
+            self.__right_term = -1
+
+    def perform(self, operation : Operations) -> None:
+        self.result = self.__operations[operation](self.__left_term, self.__right_term)
+        self.__set_flags()
+        
+
+if __name__ == "__main__":
+    alu = ALU()
+    print(alu.flags[alu.Flags.ZERO])
